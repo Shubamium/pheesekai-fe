c880867fb02f8bccf84d8102b36277f2297709dd
Vincent_Liussyaputra <liusvinv@gmail.com>
2023-11-20 03:12:44 +0700

    Fix type issue

--- src/app/artworks/page.tsx
+++ src/app/artworks/page.tsx
@@ -24,7 +24,7 @@ export default function Artworks() {
 
 
 
-type contactType = 'twitter' | 'youtube ' | 'discord ' | 'vgen' | 'kofi' | 'website';
+type contactType = 'twitter' | 'youtube' | 'discord ' | 'vgen' | 'kofi' | 'website';
 type ArtworkDisplayerProps = {
 	art:string;
 	artist:{
